410d9b2d345ce5fee2715c769ca6127a3eb1221c
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-10-08 00:03:36 +0100

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { LoginComponent } from './unauthenticated/login/login.component';
-import { RegisterComponent } from './unauthenticated/register/register.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/modules/shared.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { MainModule } from './main/modules/main.module';
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
->>>>>>> d06f9ab219eb0f3bdf68b105dc91d8f84bb77f63
+import { MaterialModule } from './shared/modules/material.module';
 
 @NgModule({
   declarations: [
@@ -22,10 +17,8 @@ import { rootRouterConfig } from './app.routes';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    AppRoutingModule,
-=======
     SharedModule,
+    MaterialModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
@@ -33,7 +26,6 @@ import { rootRouterConfig } from './app.routes';
     HttpClientModule,
     RouterModule.forChild(rootRouterConfig),
     MainModule,
->>>>>>> d06f9ab219eb0f3bdf68b105dc91d8f84bb77f63
     BrowserAnimationsModule
   ],
   providers: [],
