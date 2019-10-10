51d7d6205f8f66338bdb4076d0b7ebf75735fb14
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-10-10 21:03:45 +0100

    Change angular compiler to Ivy

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../components/header/header.component';
+import { MaterialModule } from './material.module';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     HeaderComponent
