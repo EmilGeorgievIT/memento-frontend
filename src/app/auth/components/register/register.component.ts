4676cfe6b55663006434d928854cb4bab6087687
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-10-13 12:51:08 +0100

    Add register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MyErrorStateMatcher } from 'src/app/shared/helpers/match-error-state';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  matcher = new MyErrorStateMatcher();
 
-  constructor() { }
+  hide = true;
+  hide2 = true;
+  selectedValue = 'Agent';
+
+  listRoles = [
+    "Agent",
+    "Agency",
+    "Buyer"
+  ];
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.required
+      ])],
+      email: ['', Validators.compose([
+        Validators.email,
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(8),
+        Validators.required
+      ])],
+      confirmPassword: ['', Validators.compose([
+        Validators.minLength(8),
+        Validators.required
+      ])],
+      recaptchaReactive: [null, Validators.required]
+    }, {validator: this.checkPasswords});
+  }
+  checkPasswords(group: FormGroup) {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmPassword.value;
+    return pass === confirmPass ? null : {notSame: true};
+  }
+
+  onSubmit() {
+    if(this.registerForm.invalid) {
+      return;
+    } else {
+      console.log(this.registerForm.value);  
+    }
+  }
 
   ngOnInit() {
   }
